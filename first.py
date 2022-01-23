d60007d5b0dece73758c0646b51589f8bf37bdbc
Halil Turkmen <halilturkmen@Halils-MacBook-Air.local>
2022-01-23 18:29:09 +0000

    shuffle

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ class Employee:
     self.age = age
     self.salary = 200900
 
-      
+        
 E1 = Employee("XYZ", 23, 20000)
 # E1 is the instance of class Employee.
 #__init__ allocates memory for E1. 
