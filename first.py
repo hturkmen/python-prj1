bf28b0c5d1685abd0e923cc19ae6d2a8b8e964e2
Halil <36723486+hturkmen@users.noreply.github.com>
2022-01-23 18:23:52 +0000

    Update first.py

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ class Employee:
   def __init__(self, name, age,salary):
     self.name = name
     self.age = age
-    self.salary = 20000
+    self.salary = 200900
 
       
 E1 = Employee("XYZ", 23, 20000)
