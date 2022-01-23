33010ea7644c856db9ba488502988bb2dfdf8333
Halil Turkmen <halilturkmen@Halils-MacBook-Air.local>
2022-01-23 18:19:04 +0000

    obje

--- first.py
+++ first.py
@@ -1,25 +1,13 @@
-""" Mad Libs Generator
-----------------------------------------
-"""
-#Loop back to this point once code finishes
-loop = 1
-while (loop < 10):
-# All the questions that the program asks the user
-    noun = input("Choose a noun: ")
-    p_noun = input("Choose a plural noun: ")
-    noun2 = input("Choose a noun: ")
-    place = input("Name a place: ")
-    adjective = input("Choose an adjective (Describing word): ")
-    noun3 = input("Choose a noun: ")
-## Displays the story based on the users input
-    print ("------------------------------------------")
-    print ("Be kind to your",noun,"- footed", p_noun)
-    print ("For a duck may be somebody's", noun2,",")
-    print ("Be kind to your",p_noun,"in",place)
-    print ("Where the weather is always",adjective,".")
-    print ()
-    print ("You may think that is this the",noun3,",")
-    print ("Well it is.")
-    print ("------------------------------------------")
-# Loop back to "loop = 1"
-    loop = loop + 1
+class Employee:
+  def __init__(self, name, age,salary):
+    self.name = name
+    self.age = age
+    self.salary = 20000
+
+      
+E1 = Employee("XYZ", 23, 20000)
+# E1 is the instance of class Employee.
+#__init__ allocates memory for E1. 
+print(E1.name)
+print(E1.age)
+print(E1.salary)
